172c8a93bf709659e28ef369c3848f3a21625bcb
Andrii Tarykin <ksbomj@gmail.com>
2023-03-28 20:27:25 +0300

    make linter happy

--- services/diddoc/diddoc_test.go
+++ services/diddoc/diddoc_test.go
@@ -1,3 +1,4 @@
+// nolint
 package diddoc_test
 
 import (
@@ -5,6 +6,8 @@ import (
 
 	"github.com/obada-foundation/registry/services/diddoc"
 	"github.com/obada-foundation/registry/testutil"
+	sdkdid "github.com/obada-foundation/sdkgo/did"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_Service(t *testing.T) {
@@ -12,15 +15,20 @@ func Test_Service(t *testing.T) {
 
 	service := diddoc.NewService(logger)
 
-	defer func() {
-		logger.Sync()
-	}()
-
 	t.Logf("Test \"Register\" function")
 	{
 		t.Logf("\tTest not supported DID methods")
 		{
+			notSupportedDIDs := []string{
+				`{}`,
+				`{"did":"did:bnb:1f4B9d871fed2dEcb2670A80237F7253DB5766De"}`,
+			}
 
+			for _, DID := range notSupportedDIDs {
+				err := service.Register(DID)
+				require.ErrorIs(t, err, sdkdid.ErrNotSupportedDIDMethod)
+
+			}
 		}
 
 		t.Logf("\tTest DID registration")
@@ -28,4 +36,8 @@ func Test_Service(t *testing.T) {
 			service.Register("did:obada:64925be84b586363670c1f7e5ada86a37904e590d1f6570d834436331dd3eb88)")
 		}
 	}
+
+	defer func() {
+		logger.Sync()
+	}()
 }
